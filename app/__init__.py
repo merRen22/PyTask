b7b29568eb23a1a0d5a6970d582a59fda685000a
Renato <renatomercadoluna@hotmail.com>
2021-02-15 22:29:48 -0500

    env variables for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_login import LoginManager
 from .config import Config
 from .auth import auth
 from .models import UserModel
+from firebase_admin import credentials, initialize_app
+from decouple import config
 
 login_manger = LoginManager()
 login_manger.login_view = 'auth.login'
@@ -16,6 +18,21 @@ def load_user(username):
 
 def create_app():
     app = Flask(__name__)
+
+    cred = credentials.Certificate(
+        {
+            'type': config('TYPE'),
+            'private_key': config('PRIVATE_KEY').replace('\\n', '\n'),
+            'client_email': config('CLIENT_EMAIL'),
+            'token_uri': config('TOKEN_URI')
+        }
+    )
+
+    initialize_app(
+        name='py-task',
+        credential=cred
+    )
+
     bootstrap = Bootstrap(app)
 
     app.config.from_object(Config)
